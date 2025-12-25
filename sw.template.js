01b2ec380bd2569069c3fdb401ff2614d0e84e1f
enbi0001713000 <f94z54vrd2@privaterelay.appleid.com>
2025-12-26 00:46:10 +0900

    Create sw.template.js

---
+++ sw.template.js
@@ -0,0 +1,42 @@
+/* sw.template.js
+ * Actionsが __BUILD__ を差し替えて sw.js を生成する。
+ */
+const CACHE_NAME = "school-quiz-__BUILD__";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./bank.js",
+  "./app.js",
+  "./sw.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
